43662dfe44bb36605a5739f130e34c10e9321728
Aisafa <Agsafiullin@gmail.com>
2021-04-25 16:10:45 +0300

    fixed plan

--- src/test/java/TicketBuyTest.java
+++ src/test/java/TicketBuyTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 import static com.codeborne.selenide.Selenide.open;
 
 public class TicketBuyTest {
@@ -8,7 +9,7 @@ public class TicketBuyTest {
 
     @BeforeEach
     void setUp() {
-        open("http://localhost:9999");
+        open("http://localhost:8080");
     }
 
 
